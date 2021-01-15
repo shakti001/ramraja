cf3ddae0523cb06f4853ba98bad4c25962a6d9e2
shakti001 <shaktikulshrestha98@gmail.com>
2021-01-15 16:26:20 +0530

    'ssssssssss'

--- ramraja/settings.py
+++ ramraja/settings.py
@@ -127,3 +127,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
