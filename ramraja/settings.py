44c02bcbcd5f8e35127bda5f94803b16628f0861
shakti001 <shaktikulshrestha98@gmail.com>
2021-01-15 16:29:58 +0530

    'ssssssssss'

--- ramraja/settings.py
+++ ramraja/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7(qpy2_72bil7!xkv0#i%q=ntl7d3821cm4e3rgouw=r@($(g1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','ramrajainfotech.herokuapp.com']
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ramraja.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
